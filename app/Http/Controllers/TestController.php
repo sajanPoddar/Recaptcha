203651523ba30b0642f356c7034786aee02d5831
sajan <sajanpoddar98@gmail.com>
2016-12-12 01:31:15 +0600

    guzzle require and server implementation recaptcha

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use Session;
 
 class TestController extends Controller
 {
@@ -17,8 +19,31 @@ class TestController extends Controller
     	$gender = $request->gender;
     	$token = $request->input('g-recaptcha-response');
     	if($token)
-    	{
-    		return view('layouts.name')->withName($name)->withGender($gender);
+    	{	
+    		// SERVER IMPLEMENTATION
+    		$client = new Client();
+    		$response = $client->post('https://www.google.com/recaptcha/api/siteverify', [
+    			'form_params' => array(
+    					'secret' => '6LcGYA4UAAAAAPz4Y4V4Oy6pu_RENlld_TlqdX8N',
+    					'response' => $token
+    				)
+    			]);
+    		// body httprequest and get data contents as json
+    		$results = json_decode($response->getBody()->getContents());
+    		if($results->success)
+    		{	
+    			// dd($results);
+    			Session::flash('success', 'yes, know you are human');
+    			return view('layouts.name')->withName($name)->withGender($gender);
+
+    		}
+    		else
+    		{
+    			// $results->error_codes
+    			Session::flash('error', 'you are not human');
+    			return redirect('/');
+    		}
+    		
     	}
     	else
     	{
